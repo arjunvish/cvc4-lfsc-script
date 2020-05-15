f9670f27a73f4cfd1fb57482efa86cb53d45ee2d
arjunvish <arjun-viswanathan@uiowa.edu>
2020-05-14 20:27:39 -0400

    Tested on 5 random examples and it works

--- p.sh
+++ p.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 SMTFILE=$1
-if grep -q "unsat" $SMTFILE; then
+cvc4 --dump-proofs --no-lfsc-letification $SMTFILE > result
+if [[ $(cat result | head -n 1) == "unsat" ]]; then
   echo -e "SMT problem is unsat. Getting proof...\n"
-  cvc4 --dump-proofs --no-lfsc-letification $SMTFILE | tail -n +2 >> proof.plf
+  cat result | tail -n +2 > proof.plf
+  rm result
   if [ $? -eq 0 ]; then
     echo -e "Proof in proof.plf. Checking proof...\n"
-    lfscc sigs/sat.plf sigs/smt.plf sigs/th_base.plf proof.plf
+    lfscc sigs/sat.plf sigs/smt.plf sigs/th_base.plf sigs/th_bv.plf sigs/th_bv_bitblast.plf sigs/th_arrays.plf proof.plf
+    echo -e "\n\nChecking whether assertions in proof correspond to assertions in SMT file...\n"
+    ./main.native proof.plf $SMTFILE
   else
     echo -e "Couldn't generate proof!\n"
   fi
